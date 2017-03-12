673c5e88968c17bfd87e11cfb8aa3ef17ce87cd8
Stephanos Ioannidis <root@stephanos.io>
2017-03-12 23:21:36 +0900

    LaunchDbgHost.cmd partial implementation complete.

---
+++ LaunchDbgHost.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+setlocal
+pushd %~dp0
+
+path %path%;.\App\qemu;.\App\idw
+
+qemu-system-i386 -cpu 486 -m 64 -net nic,model=pcnet -serial tcp::4444,server,nowait -hda Image\DbgHost.vhd -hdb fat:rw:C:/OpenNT/Repositories/NTOSTE.master/I386
+
+popd
+endlocal

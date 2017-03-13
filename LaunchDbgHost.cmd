c77e3e310a8115d4a3be184f9d430d03675f0c1a
Stephanos Ioannidis <root@stephanos.io>
2017-03-13 11:14:44 +0900

    Fixed invalid path reference in LaunchDbgHost.cmd and added WinDbg directory.

--- LaunchDbgHost.cmd
+++ LaunchDbgHost.cmd
@@ -5,7 +5,7 @@ pushd %~dp0
 
 path %path%;.\App\qemu;.\App\idw
 
-qemu-system-i386 -cpu 486 -m 64 -net nic,model=pcnet -serial tcp::4444,server,nowait -hda Image\DbgHost.vhd -hdb fat:rw:C:/OpenNT/Repositories/NTOSTE.master/I386
+qemu-system-i386 -cpu 486 -m 64 -net nic,model=pcnet -serial tcp::4444,server,nowait -hda Image\DbgHost.vhd -hdb fat:rw:%~dp0App\WinDbg
 
 popd
 endlocal
